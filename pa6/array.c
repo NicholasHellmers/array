90315d193438788fda4a174401cfec540466283b
Nicholas Hellmers <nihe1661@colorado.edu>
2023-10-29 17:28:44 -0600

    Added some code

--- pa6/array.c
+++ pa6/array.c
@@ -17,41 +17,41 @@
 #include <string.h>
 #include "array.h"
 
-int array_init(array *s) {                  // initialize the array
-    pthread_mutex_init(&M, NULL);
-    pthread_cond_init(&DC, NULL); /* Initialize consumer condition variable */
-    pthread_cond_init(&DP, NULL); /* Initialize producer condition variable */
+int array_init(array *s) {                      // initialize the array
+    pthread_mutex_init(&M, NULL);       
+    pthread_cond_init(&DC, NULL);               /* Initialize consumer condition variable */
+    pthread_cond_init(&DP, NULL);               /* Initialize producer condition variable */
     s->count = 0;
     return 0;
 }
 
 int array_put (array *s, char *hostname) {
-    pthread_mutex_lock(&M);	/* protect buffer */
-    while (s->count == ARRAY_SIZE)		       /* If there is something in the buffer then wait */
+    pthread_mutex_lock(&M);	                    /* protect buffer */
+    while (s->count == ARRAY_SIZE)		        /* If there is something in the buffer then wait */
         pthread_cond_wait(&DP, &M);
 
     strcpy(s->buffer[s->count], hostname);
     s->count++;
     printf("Producer wrote %d\n", hostname);
-    pthread_cond_signal(&DC);	/* wake up consumer */
-    pthread_mutex_unlock(&M);	/* release the buffer */
+    pthread_cond_signal(&DC);	                /* wake up consumer */
+    pthread_mutex_unlock(&M);	                /* release the buffer */
     pthread_exit(0);
 }
 
 int array_get (array *s, char **hostname) {
-    pthread_mutex_lock(&M);	/* protect buffer */
-    while (s->count == 0)			/* If there is nothing in the buffer then wait */
+    pthread_mutex_lock(&M);	                    /* protect buffer */
+    while (s->count == 0)			            /* If there is nothing in the buffer then wait */
         pthread_cond_wait(&DC, &M);
     s->count--;
     strcpy(*hostname, s->buffer[s->count]);
     printf("Consumer reads %d\n", *hostname);
-    pthread_cond_signal(&DP);	/* wake up producer */
-    pthread_mutex_unlock(&M);	/* release the buffer */
+    pthread_cond_signal(&DP);	                /* wake up producer */
+    pthread_mutex_unlock(&M);	                /* release the buffer */
     pthread_exit(0);
 }
 
-void array_free(array *s) {                 // free the array's resources
-    pthread_mutex_destroy(&M);	/* Free up M mutex */
-    pthread_cond_destroy(&DC);	/* Free up consumer condition variable */
-    pthread_cond_destroy(&DP);	/* Free up producer condition variable */
+void array_free(array *s) {                     // free the array's resources
+    pthread_mutex_destroy(&M);	                /* Free up M mutex */
+    pthread_cond_destroy(&DC);	                /* Free up consumer condition variable */
+    pthread_cond_destroy(&DP);	                /* Free up producer condition variable */
 }
--- pa6/multi-lookup.h
+++ pa6/multi-lookup.h
@@ -12,41 +12,27 @@
 #include<sys/time.h>
 #include<pthread.h>
 
+#define MAX_INPUT_FILES 100             // Maximum number of hostname file arguments allowed
+#define MAX_REQUESTER_THREADS 10        //- Maximum number of concurrent requester threads allowed
+#define MAX_RESOLVER_THREADS 10         //- Maximum number of concurrent resolver threads allowed
+#define MAX_IP_LENGTH INET6_ADDRSTRLEN  // INET6_ADDRSTRLEN is the maximum size IP address string util.cs
 
-/* This header file is to simply define the structure of the shared buffer
-and consumers and producers. The pool of requesters and resolvers will
-be created in multi-llokup.c*/
-#define MAX_INPUT_FILES 100 // Maximum number of hostname file arguments allowed
-#define MAX_REQUESTER_THREADS 10 //- Maximum number of concurrent requester threads allowed
-#define MAX_RESOLVER_THREADS 10  //- Maximum number of concurrent resolver threads allowed
-#define MAX_IP_LENGTH INET6_ADDRSTRLEN // INET6_ADDRSTRLEN is the maximum size IP address string util.cs
+#define THREAD_NUM MAX_REQUESTER_THREADS + MAX_RESOLVER_THREADS
 
-#define THREAD_NUM (MAX_REQUESTER_THREADS+MAX_RESOLVER_THREADS)
-
-//************* Shared Buffer Struct ************//
-
-                //defined in array.h//
-
-
-//*************Requester Struct***********//
 typedef struct Requesters{
-
-    char** input_files; //shared access to list input files (names)
-    FILE * serviced_log; //argv[3]
+    char** input_files;                 //shared access to list input files (names)
+    FILE * serviced_log;                //argv[3]
     pthread_mutex_t file_position_lock; //provide mutual exclusion for writing
-    pthread_mutex_t serviced_lock; //writing log
-    array * shared_buffer; //pointer to stack struct
-    int global_index; //the key to multithreaded file reads
-    int file_count; //argc-5
-
+    pthread_mutex_t serviced_lock;      //writing log
+    array * shared_buffer;              //pointer to stack struct
+    int global_index;                   // the key to multithreaded file reads
+    int file_count;                     // argc-5
 }Requesters;
 
-//*************Resolver Struct***********//
-
 typedef struct Resolvers{
-    pthread_mutex_t results_lock; //provide mutual exclusion for writing to results.txt
+    pthread_mutex_t results_lock;       //provide mutual exclusion for writing to results.txt
     array * shared_buffer;
-    FILE * results_log; //argv[4]
+    FILE * results_log;                 //argv[4]
 }Resolvers;
 
 void* requesterTread(void* inputfile);
